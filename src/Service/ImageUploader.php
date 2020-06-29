4372fd69b07bb02c87fe51bd252ee1644e3a9f66
Damienbelingheri <belingheridamien@gmail.com>
2020-06-29 20:02:32 +0200

    add upload avatar API + service

--- src/Service/ImageUploader.php
+++ src/Service/ImageUploader.php
@@ -3,26 +3,46 @@
 namespace App\Service;
 
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ImageUploader
 {
-    public function getRandomFileName(string $extension): string
+
+    private $targetDirectory;
+    private $slugger;
+
+    public function __construct($targetDirectory, SluggerInterface $slugger)
     {
-        return preg_replace('/[+=\/]/', random_int(0, 9), base64_encode(random_bytes(8))).'.'.$extension;
+        $this->targetDirectory = $targetDirectory;
+        $this->slugger = $slugger;
     }
 
-    public function moveFile(?UploadedFile $imageFile, string $targetDirectory): ?string
+
+    public function getRandomFileName(string $extension): string
+    {
+        return preg_replace('/[+=\/]/', random_int(0, 9), base64_encode(random_bytes(8))) . '.' . $extension;
+    }
+
+    public function avatarFile(?UploadedFile $imageFile, string $targetDirectory): ?string
     {
         if ($imageFile != null) {
             // Créons un nouveau nom de fichier
             // On utilise un petit algorithme fait maison qui nous fait une chaine de caractère aléatoire
             $filename = $this->getRandomFileName($imageFile->getClientOriginalExtension());
 
-            $imageFile->move('images/'.$targetDirectory, $filename);
+            $imageFile->move($this->getTargetDirectory(), $filename);
 
             return $filename;
         } else {
+
             return null;
         }
+    }
+
+
+    public function getTargetDirectory()
+    {
+        return $this->targetDirectory;
     }
 }
