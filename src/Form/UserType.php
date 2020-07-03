109756e42c015616758249625e7829a68e931c45
Florian salducci <Wj3kilah6n>
2020-07-03 11:27:30 +0200

    comment

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -39,7 +39,7 @@ class UserType extends AbstractType
                     'multiple' => true,
                     'expanded' => true,
                 ])
-                ->add('avatar')
+
             /* ->add('avatar', FileType::class, [
                 'label' => 'avatar',
                 'mapped' => true,
--- src/Service/ImageUploader.php
+++ src/Service/ImageUploader.php
@@ -3,12 +3,10 @@
 namespace App\Service;
 
 use Symfony\Component\HttpFoundation\File\UploadedFile;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ImageUploader
 {
-
     private $targetDirectory;
     private $slugger;
 
@@ -18,31 +16,23 @@ class ImageUploader
         $this->slugger = $slugger;
     }
 
-
     public function getRandomFileName(string $extension): string
     {
-        return preg_replace('/[+=\/]/', random_int(0, 9), base64_encode(random_bytes(8))) . '.' . $extension;
+        return preg_replace('/[+=\/]/', random_int(0, 9), base64_encode(random_bytes(8))).'.'.$extension;
     }
 
     public function avatarFile(?UploadedFile $imageFile, string $targetDirectory): ?string
     {
         if ($imageFile != null) {
-            // Créons un nouveau nom de fichier
-            // On utilise un petit algorithme fait maison qui nous fait une chaine de caractère aléatoire
+            // Let's create a new filename
+            //  We use a little homemade algorithm that makes a random string of characters
             $filename = $this->getRandomFileName($imageFile->getClientOriginalExtension());
 
-            $imageFile->move($this->getTargetDirectory(), $filename);
+            $imageFile->move('images/'.$targetDirectory, $filename);
 
             return $filename;
         } else {
-
             return null;
         }
-    }
-
-
-    public function getTargetDirectory()
-    {
-        return $this->targetDirectory;
     }
 }
